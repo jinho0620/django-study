2aa20f9252315d99b212682eb36f79ef8807877c
jinho0620 <jinhojo0620@gmail.com>
2024-12-17 17:46:11 +0900

    [Add] comment why we need while loop when getting data

--- socket1.py
+++ socket1.py
@@ -5,9 +5,9 @@ mysock.connect(("data.pr4e.org", 80))
 cmd = 'GET http://data.pr4e.org/page1.htm HTTP/1.0\r\n\r\n'.encode()
 mysock.send(cmd)
 
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
+while True: # Loop is necessary since how much data we will get from the server
+    data = mysock.recv(512) # Read up to 512 bytes from the buffered data
+    if len(data) < 1: # If no data is coming in, stop reading
         break
     print(data.decode(), end="")
 
